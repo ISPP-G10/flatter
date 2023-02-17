3d60bda5b1df9c74b8bbba60d2c8c6b7c582984f
Alex-GF <yalejandro9@gmail.com>
2023-02-17 11:39:08 +0100

    FEAT: ZOOMING IMAGES
    Final version of the iamgesZoom effect

--- client/src/components/imagesZoom.js
+++ client/src/components/imagesZoom.js
@@ -1,39 +1,58 @@
 import '../static/css/images-zoom.css';
 import { useEffect, useState } from "react";
 
-const ZoomingImages = () => {
+const ZoomingImages = ({imageSet}) => {
 
     let [spiralPoints, setSpiralPoints] = useState([]);
 
-    const imageSet = [
-        require("../static/files/images/sample-1.jpg"),
-        require("../static/files/images/sample-2.jpg"),
-        require("../static/files/images/sample-3.jpg"),
-        require("../static/files/images/sample-4.jpg"),
-        require("../static/files/images/sample-5.jpg"),
-        require("../static/files/images/sample-6.jpg"),
-        require("../static/files/images/sample-7.jpg"),
-        require("../static/files/images/sample-8.jpg"),
-        require("../static/files/images/sample-9.jpg"),
-        require("../static/files/images/sample-10.jpg"),
+    const THETAS = [
+        7*Math.PI/4,
+        3*Math.PI/4,
+        Math.PI/4,
+        5*Math.PI/4,
+        0,
+        Math.PI,
+        Math.PI/2,
+        3*Math.PI/2,
     ];
+
+    function generateSpritalPoints(){
+
+        let spiralPoints = [];  
+        let directionVectors = []
+        let modules = [];
+        let numberOfPictures = imageSet.length;
+    
+        if(numberOfPictures > 8){
+            let directionVectorsGenerationCounter = 0;
+            for(let i=0; i<numberOfPictures; i++){
+                directionVectors.push(THETAS[directionVectorsGenerationCounter]);
+                directionVectorsGenerationCounter++;
+                if(directionVectorsGenerationCounter === 8){
+                    directionVectorsGenerationCounter = 0;
+                }
+            }
+        }else{
+            directionVectors = THETAS;
+        }
+
+        for(let i=1; i<=numberOfPictures; i++){
+            modules.push((200/i)+100);
+        }
+    
+        for(let i=0; i<numberOfPictures; i++){
+            let x = modules[i]*Math.cos(directionVectors[i]);
+            let y = modules[i]*Math.sin(directionVectors[i]);
+            spiralPoints.push({x: x, y: y});
+        }
+
+        return spiralPoints;
+    
+    }
 
     useEffect(() => {
 
-        let points = [];
-        const a = 10;
-        const b = 10;
-        const k = 8;
-
-        for(let i=0; i<imageSet.length; i++){
-            let theta =  i*Math.PI/4;
-            let r = a + b * Math.cos(k*theta)
-            let x = r*Math.cos(theta);
-            let y = r*Math.sin(theta);
-            points.push({x,y});
-        }
-
-        setSpiralPoints(points);
+        setSpiralPoints(generateSpritalPoints());
 
         document.addEventListener("scroll", (e) => {
             let value = window.scrollY
@@ -41,25 +60,28 @@ const ZoomingImages = () => {
 
             zoomingImages.forEach((image, index) => {
 
-                let previousPerspective = image.style.transform.split(" ")[0];
-                let previousXTransform = image.style.transform.split(" ")[1];
+                let splittedTransform = image.style.transform.split(" ");
 
-                image.style.transform = `${previousPerspective} ${previousXTransform} translateZ(${value}px)`;
+                let previousPerspective = splittedTransform[0];
+                let previousXTransform = splittedTransform[1];
+                let previousYTransform = splittedTransform[2];
+
+                image.style.transform = `${previousPerspective} ${previousXTransform} ${previousYTransform} translateZ(${value}px)`;
             });
         });
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
     return(
         <div className="component-wrapper">
-            <div className="zooming-images-container" style={{height: `${(imageSet.length-2)*1000}px`}}>
+            <div className="zooming-images-container" style={{height: `${(imageSet.length)*750+1500}px`, marginTop: '300px'}}>
             {
                 (spiralPoints.length > 0 && imageSet) && imageSet.map((image, index) => {
 
                     let imageStyle = {
-                        top: `${(10*spiralPoints[index].y) + 300}px`,
-                        height: `${((imageSet.length+3)*30)/(index+1)}px`,
-                        width: `${((imageSet.length+3)*30)/(index+1)}px`,
-                        transform: `perspective(${((index+1)*500)+1000}px) translateX(${10*spiralPoints[index].x}px)`,
+                        maxHeight: `${250/(index+1)+125}px`,
+                        maxWidth: `${250/(index+1)+125}px`,
+                        transform: `perspective(${((index+1)*750)+1000}px) translateX(${spiralPoints[index].x}px) translateY(${spiralPoints[index].y}px)`,
                         zIndex: `${10000-index}`
                     }
 
@@ -71,6 +93,6 @@ const ZoomingImages = () => {
             </div>
         </div>
     );
-} 
+}
 
 export default ZoomingImages;
