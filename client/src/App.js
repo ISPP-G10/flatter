2659233238fce3c27e331f3b045eb0f3c73f53a0
javiercavlop <javiercavlop@gmail.com>
2023-03-05 20:58:49 +0100

    FEAT: DISEÑO CHATS
    El componente de los chats ha sido diseñado

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,16 @@
+import "./static/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './pages/landingPage';
 import Error from './pages/error';
 import './static/css/globals.css';
 import Ejemplo from './pages/ejemploIntegracion';
+import Chat from './sections/chat';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route exact path="/" element={<LandingPage/>}/>
+        <Route exact path="/" element={<> <Chat /> <LandingPage/> </>}/>
         <Route path="/prueba" element={<Ejemplo/>}/>
         <Route path="*" element={<Error/>}/>
       </Routes>
---
+++ client/src/components/chat/message.js
@@ -0,0 +1,62 @@
+import PropTypes from "prop-types";
+
+
+const Message = (props) => {
+
+    return (
+        <>
+            {
+                props.mine ?
+                    (
+                        <div className={`message ${props.last} d-flex justify-content-center align-items-center`}>
+                            {props.message}
+                            <div className="d-flex flex-column justify-content-center align-items-center ml-2">
+                                <img className="class-message-profile" style={{display: props.displayPic }} src={props.profilePic} alt=""/>
+                                <span className="class-message-time">{props.time}</span>
+                            </div>
+                        </div>
+                    )
+                    :
+                    (
+                        <div className={`message d-flex ${props.last} justify-content-center align-items-center`}>
+                            <div className="d-flex justify-content-between align-items-center">
+                                <div className="d-flex flex-column justify-content-center align-items-center mr-2">
+                                    <img className="class-message-profile-left" style={{display: props.displayPic }} src={props.profilePic} alt=""/>
+                                    <span className="class-message-time-left">{props.time}</span>
+                                </div>
+                                <div className="d-flex flex-column justify-content-center align-items-center">
+                                    <span className="class-chat-user" style={{display: props.displayUser }}>{props.user}</span>
+                                    {props.message}
+                                </div>
+                            </div>
+                        </div>
+                    )
+            }
+        </>
+    );
+
+}
+
+Message.propTypes = {
+    message: PropTypes.string,
+    profilePic: PropTypes.string,
+    time: PropTypes.string,
+    last: PropTypes.string,
+    user: PropTypes.string,
+    displayUser: PropTypes.string,
+    displayPic: PropTypes.string,
+    mine: PropTypes.bool
+}
+
+Message.defaultProps = {
+    message: "",
+    profilePic: "",
+    time: "",
+    last: "",
+    user: "",
+    displayUser: "none",
+    displayPic: "none",
+    mine: false
+}
+
+export default Message;
