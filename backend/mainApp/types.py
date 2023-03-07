2db9dceddbd94f0eca5fdf7db3d1291602eef9ec
Patri17 <64139089+Patri17@users.noreply.github.com>
2023-03-07 19:33:33 +0100

    Update types.py

--- backend/mainApp/types.py
+++ backend/mainApp/types.py
@@ -1,10 +1,5 @@
 from graphene_django.types import DjangoObjectType
-<<<<<<< Updated upstream
-from mainApp.models import Property
-=======
-
 from .models import Property
->>>>>>> Stashed changes
 
 class PropertyType(DjangoObjectType):
   class Meta:
