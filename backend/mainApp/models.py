2f851b4e51c0d450ff63ff5de2b16b9bbb1935d5
javpacmar <javpacmar@alum.us.es>
2023-03-09 21:17:12 +0100

    FEAT: INTEGRACIÓN DETALLE VIVIENDA
    Se ha añadido flatmates al modelo de Property.
    Se ha integrado la API con el frontend.
    Se ha extendido los datos de propiedades.
    
    (#72)

--- backend/mainApp/models.py
+++ backend/mainApp/models.py
@@ -24,6 +24,7 @@ class Property(models.Model):
     tags = models.ManyToManyField(Tag, related_name=_('property_tags'))
     images = models.ManyToManyField(Image)
     owner = models.ForeignKey(FlatterUser, related_name=_('property_owner'), on_delete=models.CASCADE)
+    flatmates = models.ManyToManyField(FlatterUser, related_name=_('property_flatmates'))
 
 class Review(models.Model):
 
