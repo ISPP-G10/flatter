2e5ca1ed049d691371118b243352928d4db77b79
javiercavlop <javiercavlop@gmail.com>
2023-03-09 12:56:54 +0100

    FEAT: PROFILE LISTO PARA INTEGRAR
    Se ha dejado el componente listo para integrar

--- client/src/components/profile/publicProfileCard.js
+++ client/src/components/profile/publicProfileCard.js
@@ -1,20 +1,27 @@
 import '../../static/css/components/publicProfileCard.css'
 import Tag from '../tag';
+import PropTypes from 'prop-types';
 
-const PublicProfileCard = () => {
+const PublicProfileCard = (props) => {
 
     return (
-        <div className='profile-card-container'>
+        <div className={`profile-card-container ${props.isMe ? 'profile-card-me' : props.isTenant ? 'profile-card-tenant' : 'profile-card-propietary'}`}>
             <div className="profile-card-info">
-                <h2>Lucía Martín</h2>
-                <p>Estudiante en Universidad de Sevilla</p>
-                <p>24 años</p>    
+                <div className="profile-card-data">
+                    <div className={`profile-card-edit ${props.isMe ? '' : 'no-edit'}`}>
+                        <h2>{props.name}</h2>
+                        <button className="profile-card-btn" title="Edita tu perfil"></button>
+                    </div>
+                    <p>{props.job}</p>
+                    <p>{props.age} años</p> 
+                </div>
             </div>
             <div className='profile-card-details'>
                 <img className='profile-card-img' src={require('../../static/files/images/foto.jpg')} alt='Profile'/>
                 <div className='profile-card-bio'>
                     <h2>Yo...</h2>
-                    <p class="profile-card-description">Soy una estudiante a la que le pasárselo bien pero que también es responsable.
+                    <p className="profile-card-description">
+                        {props.bio}
                     </p>
                     <div className='tags-container'>
                         <Tag name='LOL' color='#f783de' />
@@ -24,6 +31,24 @@ const PublicProfileCard = () => {
             </div>           
         </div>
     );
+}
+
+PublicProfileCard.propTypes = {
+    name: PropTypes.string,
+    job: PropTypes.string,
+    age: PropTypes.number,
+    bio: PropTypes.string,
+    isMe: PropTypes.bool,
+    isTenant: PropTypes.bool
+}
+
+PublicProfileCard.defaultProps = {
+    name: "",
+    job: "",
+    age: 0,
+    bio: "",
+    isMe: false,
+    isTenant: true
 }
 
 export default PublicProfileCard;
--- client/src/pages/publicProfile.js
+++ client/src/pages/publicProfile.js
@@ -7,9 +7,9 @@ import PublicProfileCard from "../components/profile/publicProfileCard";
 const PublicProfile = () => {
 
     return(
-        <FlatterPage withBackground >
-            <div class="profile-grid">
-                <PublicProfileCard/>
+        <FlatterPage withBackground userLogged>
+            <div className="profile-grid">
+                <PublicProfileCard name="Lucía Martín" job="Estudiante en Universidad de Sevilla" age={24} bio="Soy una estudiante a la que le pasárselo bien pero que también es responsable." isMe={true} isTenant={true}/>
                 <span>Pruebas</span>
                 <CommentsBox />
             </div>
