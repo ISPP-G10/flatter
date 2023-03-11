fe65a78576a22b042c8c6694d5fc922d48836d0f
javiercavlop <javiercavlop@gmail.com>
2023-03-11 10:53:49 +0100

    FIX: DESPLIEGUE
    Probando a arreglar el despliegue

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -13,7 +13,7 @@ fi
 python manage.py collectstatic --noinput
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
-python manage.py loaddata testers
+python manage.py loaddata testers.json
 echo "from authentication.models import FlatterUser;
 FlatterUser.objects.filter(email='$DJANGO_ADMIN_EMAIL').delete();
 FlatterUser.objects.create_superuser('$DJANGO_ADMIN_USER', '$DJANGO_ADMIN_EMAIL', '$DJANGO_ADMIN_PASSWORD')" | python manage.py shell
