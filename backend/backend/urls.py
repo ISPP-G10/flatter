59b4e4d162c8b218ab0709d8418f51ff8455185b
Alex-GF <yalejandro9@gmail.com>
2023-03-14 23:18:07 +0100

    FEAT: DOCKER COMPOSE ARREGLADO Y PREPARADO PARA DESPLIEGUE
    No se incluye en este commit el archivo .env necesario para lanzar el docker-compose. Se configurará en futuros commits como secrets

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -4,8 +4,17 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
+from django.views.static import serve 
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    urlpatterns = [
+        path('admin/', admin.site.urls),
+        path('api/graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+else:
+    urlpatterns = [
+        path('admin/', admin.site.urls),
+        path('api/graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
+        path('api/media', serve, {'document_root': settings.MEDIA_ROOT}),
+        path('api/static', serve, {'document_root': settings.STATIC_ROOT})
+    ]
