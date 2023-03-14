f9f53e9a380318f411e4f868dbb7e7df7bb4b777
Alex-GF <yalejandro9@gmail.com>
2023-03-14 15:43:49 +0100

    FEAT: TESTS MÓDULO AUTENTICACIÓN
    Se añaden los tests diseñados por Alba Tinajero y Carmen García durante el sprint 1 y que no fueron fusionados con la rama de integración final antes del comienzo de este proceso.

--- backend/authentication/tests.py
+++ backend/authentication/tests.py
@@ -1,3 +1,4 @@
+from django.db import IntegrityError
 from django.test import TestCase
 from .models import FlatterUser, UserPreferences, Tag
 
@@ -22,4 +23,56 @@ class MyTest(TestCase):
         
         assert FlatterUser.objects.count() == 1
         assert FlatterUser.objects.first().tags.count() == 2
+        
+    def test_duplicate_username(self):
+
+        user1 = FlatterUser.objects.create_user(
+                            username="Hola", 
+                            password="1234", 
+                            email="asd1@asd.asd", 
+                            first_name="A", 
+                            last_name="B", 
+                            genre = 'H',
+                            flatter_coins = 0,
+                        )
+
+        try:
+            user2 = FlatterUser.objects.create_user(
+                            username="Hola", 
+                            password="1234", 
+                            email="asd2@asd.asd", 
+                            first_name="C", 
+                            last_name="D", 
+                            genre = 'M',
+                            flatter_coins = 0,
+                        )
+            self.fail("Se esperaba un error por duplicación de nombre de usuarios.")
+        except IntegrityError:
+            pass
+
+    def test_duplicate_email(self):
+
+        user1 = FlatterUser.objects.create_user(
+                            username="Hola", 
+                            password="1234", 
+                            email="asd1@asd.asd", 
+                            first_name="A", 
+                            last_name="B", 
+                            genre = 'H',
+                            flatter_coins = 0,
+                        )
+
+        try:
+            user2 = FlatterUser.objects.create_user(
+                            username="Hola2", 
+                            password="1234", 
+                            email="asd1@asd.asd", 
+                            first_name="C", 
+                            last_name="D", 
+                            genre = 'M',
+                            flatter_coins = 0,
+                        )
+            self.fail("Se esperaba un error por duplicación de email.")
+        except IntegrityError:
+            pass
         
