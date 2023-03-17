61cfc5c476cc451d4b9e218bb98c1be6245c91d9
javpacmar <javpacmar@alum.us.es>
2023-03-17 10:51:33 +0100

    FIX: ARREGLA ÚLTIMO LINK DE FOOTER

--- client/src/sections/footer.js
+++ client/src/sections/footer.js
@@ -31,11 +31,11 @@ const Footer = () => {
                 <li className='menu-item'><Link className='menu-link' to="/search">Buscador de viviendas</Link></li>
                 <li className='menu-item'><Link className='menu-link' to="/users?owner=false">Buscador de compañeros</Link></li>
                 <li className='menu-item'><Link className='menu-link' to="/users?owner=true">Buscador de propietarios</Link></li>
-                <li className='menu-item'><Link className='menu-link' to="/privacy">Aviso de privacidad</Link></li>
                 {
                     localStorage.getItem("roles") && localStorage.getItem("roles").includes("OWNER") &&
-                    <li><Link to="/properties">Mis viviendas</Link></li>
+                    <li className='menu-item'><Link className='menu-link' to="/properties">Mis viviendas</Link></li>
                 }
+                <li className='menu-item'><Link className='menu-link' to="/privacy">Aviso de privacidad</Link></li>       
             </ul>
             <p>&copy; 2023 Flatter | Todos los derechos reservados</p>
         </footer>
