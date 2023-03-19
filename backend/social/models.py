5d645e894da68244f225ea000149b2a8854f73dc
Jose Luis Garcia Marin <josgarmar31@alum.us.es>
2023-03-19 15:18:51 +0100

    FEAT: SIGNALS DE LOS MODELOS DE LOS GRUPOS, MENSAJES Y USUARIOS
    SE HAN IMPLEMENTADO MÁS RESTRICCIONES A LOS MODELOS DEL MODULO SOCIAL

--- backend/social/models.py
+++ backend/social/models.py
@@ -1,16 +1,17 @@
 from django.db import models
 from authentication.models import FlatterUser
-from django.db.models.signals import post_save, pre_save, pre_delete
+from django.core.exceptions import ValidationError
+from django.db.models.signals import post_save, pre_save, pre_delete, m2m_changed
 from django.dispatch import receiver
 
 class Group(models.Model):
-    name = models.CharField(max_length=30, blank=False, null=True)
+    name = models.CharField(max_length=30, blank=False, null=False)
     individual = models.BooleanField(default=False)
     users = models.ManyToManyField(FlatterUser)
 
 
 class Message(models.Model):
-    text = models.TextField(max_length=500, blank=False, null=True)
+    text = models.TextField(max_length=500, blank=False, null=False)
     timestamp = models.DateTimeField(auto_now_add=True, null=False)
     user = models.ForeignKey(FlatterUser, on_delete=models.CASCADE)
     group = models.ForeignKey(Group, on_delete=models.CASCADE)
@@ -33,21 +34,41 @@ def add_message(sender, instance, created, **kwargs):
 @receiver(pre_save, sender=Message)
 def check_user_in_group(sender, instance, **kwargs):
     if instance.user not in instance.group.users.all():
-        raise Exception('User not in group')
+        raise ValidationError('User not in group')
 
 
-# Comrpobar que un grupo individual solo tenga un usuario
-@receiver(pre_save, sender=Group)
-def check_individual_group(sender, instance, **kwargs):
-    if instance.individual:
-        if instance.users.count() > 1:
-            raise Exception('Individual group can only have one user')
+# Comrpobar que un grupo individual solo tenga dos usuarios
+@receiver(m2m_changed, sender=Group.users.through)
+def check_individual_group(sender, instance, action, **kwargs):
+    if instance.individual and action == 'post_add' and instance.users.count() != 2:
+        raise ValidationError('Individual group can only have two users')
 
 # No eliminar un grupos no individuales si tienen más de tres usuarios
 @receiver(pre_delete, sender=Group)
 def check_group(sender, instance, **kwargs):
-    if not instance.individual and instance.users.count() > 3:
-        raise Exception('Group can not be deleted')
+    if not instance.individual and instance.users.count() > 2:
+        raise ValidationError('Group can not be deleted')
+
+
+
+#No permitir la creación de grupos no individuales con menos de 3 usuarios.
+@receiver(m2m_changed, sender=Group.users.through)
+def check_group(sender, instance, action, **kwargs):
+    if not instance.individual and action == 'post_add':
+        if instance.users.count() < 3:
+            raise ValidationError('Group must have at least 3 users')
+
+
+#Eliminar automáticamente los chats individuales
+@receiver(m2m_changed, sender=Group.users.through)
+def check_groups(sender, instance, action, **kwargs):
+    if instance.individual and instance.users.count() < 2 and action == 'post_remove':
+        instance.delete()
+    if not instance.individual and instance.users.count() < 3 and action == 'post_remove':
+        instance.delete()
+
+
+
 
 
 
