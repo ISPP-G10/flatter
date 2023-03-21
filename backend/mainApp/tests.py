3b2154e6c6cee674497e46e607d0e3d42cebe531
Fraborcar1 <80266204+Fraborcar1@users.noreply.github.com>
2023-03-21 19:25:15 +0100

    TEST: REFACTORIZACIÓN DE CÓDIGO
    Se ha creado una clase setup que contenga las variables necesarias para cada test, para evitar el código clonado

--- backend/mainApp/tests.py
+++ backend/mainApp/tests.py
@@ -1,24 +1,26 @@
+import json
+import graphene
+from graphene_django.utils.testing import GraphQLTestCase
 from django.db import IntegrityError
 from django.test import TestCase
-from authentication.models import FlatterUser, UserPreferences, Tag
+from authentication.models import FlatterUser, Tag
 from .models import Property
+
 
 # Create your tests here.
 class TestProperty(TestCase):
-    
-    def test_createPropertyPositive(self):
-        
-        user = FlatterUser.objects.create_user(
-                            username="Hola", 
-                            password="1234", 
-                            email="asd@asd.asd", 
-                            first_name="A", 
-                            last_name="B", 
-                            genre = 'H',
-                            flatter_coins = 0,
-                        )
-        
-        Property.objects.create(
+    def setUp(self):
+        self.user = FlatterUser.objects.create_user(
+            username="Hola",
+            password="1234",
+            email="asd@asd.asd",
+            first_name="A",
+            last_name="B",
+            genre='H',
+            flatter_coins=0,
+        )
+
+        self.property = Property.objects.create(
             title="title",
             description="description",
             bedrooms_number=3,
@@ -27,25 +29,18 @@ class TestProperty(TestCase):
             location="location",
             province="province",
             dimensions=200,
-            owner=user,
+            owner=self.user,
             max_capacity=5,
         )
+    
+    def test_createPropertyPositive(self):
         
         assert Property.objects.count() == 1
 
     def test_createPropertyNegative(self):
         
-        user = FlatterUser.objects.create_user(
-                            username="Hola", 
-                            password="1234", 
-                            email="asd@asd.asd", 
-                            first_name="A", 
-                            last_name="B", 
-                            genre = 'H',
-                            flatter_coins = 0,
-                        )
         try:
-            Property.objects.create(
+            self.property = Property.objects.create(
                 title="title",
                 description="description",
                 bedrooms_number=3,
@@ -54,10 +49,10 @@ class TestProperty(TestCase):
                 location="location",
                 province="province",
                 dimensions=200,
-                owner=user,
+                owner=self.user,
                 max_capacity=-5,
             ) 
-            self.fail("Se esperaba un error por duplicación de email.")
+            self.fail("Se esperaba un error por creación de inmueble")
         except IntegrityError:
             pass
     
@@ -65,58 +60,13 @@ class TestProperty(TestCase):
 
         tag_1 = Tag.objects.create(name="Tag 1", entity="U")
         
-        user = FlatterUser.objects.create_user(
-                            username="Hola", 
-                            password="1234", 
-                            email="asd@asd.asd", 
-                            first_name="A", 
-                            last_name="B", 
-                            genre = 'H',
-                            flatter_coins = 0,
-                        )
-        
-        property = Property.objects.create(
-            title="title",
-            description="description",
-            bedrooms_number=3,
-            bathrooms_number=2,
-            price=100,
-            location="location",
-            province="province",
-            dimensions=200,
-            owner=user,
-            max_capacity=5,
-        )
-        property.tags.add(tag_1)
+        self.property.tags.add(tag_1)
         
         assert Property.objects.first().tags.count() == 1
     
     def test_updateProperty(self):
         
-        user = FlatterUser.objects.create_user(
-                            username="Hola", 
-                            password="1234", 
-                            email="asd@asd.asd", 
-                            first_name="A", 
-                            last_name="B", 
-                            genre = 'H',
-                            flatter_coins = 0,
-                        )
-        
-        Property.objects.create(
-            title="title",
-            description="description",
-            bedrooms_number=3,
-            bathrooms_number=2,
-            price=100,
-            location="location",
-            province="province",
-            dimensions=200,
-            owner=user,
-            max_capacity=5,
-        )
-        
-        Property.objects.update(
+        self.property = Property.objects.update(
             title="Mi habitación",
             description="un día bonito",
         )
@@ -125,63 +75,15 @@ class TestProperty(TestCase):
     
     def test_deleteProperty(self):
         
-        user = FlatterUser.objects.create_user(
-                            username="Hola", 
-                            password="1234", 
-                            email="asd@asd.asd", 
-                            first_name="A", 
-                            last_name="B", 
-                            genre = 'H',
-                            flatter_coins = 0,
-                        )
-        
-        Property.objects.create(
-            title="title",
-            description="description",
-            bedrooms_number=3,
-            bathrooms_number=2,
-            price=100,
-            location="location",
-            province="province",
-            dimensions=200,
-            owner=user,
-            max_capacity=5,
-        )
-        
-        property = Property.objects.get(title="title")
-        property.delete()
+        self.property = Property.objects.get(title="title")
+        self.property.delete()
 
         assert Property.objects.count() == 0
     
     def test_MakePropertyOutstanding(self):
-
-        user = FlatterUser.objects.create_user(
-                            username="Hola", 
-                            password="1234", 
-                            email="asd@asd.asd", 
-                            first_name="A", 
-                            last_name="B", 
-                            genre = 'H',
-                            flatter_coins = 0,
-                        )
         
-        Property.objects.create(
-            title="title",
-            description="description",
-            bedrooms_number=3,
-            bathrooms_number=2,
-            price=100,
-            location="location",
-            province="province",
-            dimensions=200,
-            owner=user,
-            max_capacity=5,
-        )
-
-        Property.objects.update(
+        self.property = Property.objects.update(
             is_outstanding=True
         )
 
         assert Property.objects.get(title="title").is_outstanding == True
-
-        
