62790f738a90b3e7a253daa9e1d7f2b3e755396c
Alex-GF <yalejandro9@gmail.com>
2023-03-24 10:11:48 +0100

    FEAT: MEJORAS PARA DESPLIEGUE ASGI

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -3,7 +3,8 @@ import os
 from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+environment = os.environ.get("DJANGO_ENV", "development")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings.' + environment)
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
