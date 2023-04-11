ee10a2d5f1b75ca830a3c52af35d84e8e18eeb7a
Jose Luis Garcia Marin <josgarmar31@alum.us.es>
2023-04-11 13:43:39 +0200

    REFACTOR: REFACTORIZACIÓN DEL CÓDIGO
     REFACTORIZACIÓN DEL CÓDIGO

--- backend/social/recommendations.py
+++ backend/social/recommendations.py
@@ -17,7 +17,10 @@ def user_age(birthday):
 def jaccard_similarity(set1, set2):
     intersection = len(set1.intersection(set2))
     union = len(set1.union(set2))
-    return intersection / union
+    if union == 0:
+        return 1.0
+    else:
+        return intersection / union
 
 
 
@@ -25,11 +28,6 @@ def jaccard_similarity(set1, set2):
 def build_similarity_matrix(users, user_login):
     # Inicializar la matriz de similitud
     similarity_list = dict()
-    for user in users:
-        similarity_list[user.id] = 0
-
-
-
 
     # Calcular la similitud entre los conjuntos de etiquetas de cada par de usuarios
 
@@ -44,11 +42,11 @@ def build_similarity_matrix(users, user_login):
             dist_euc = int(euclidean_distances([[age1]], [[age2]])[0][0])
             dist_norm = dist_euc / 100
             similitud_age = math.exp(-1 *  dist_norm)
-            similaruty_total = similarity_tags*0.7 + similitud_age*0.3
+            similarity_total = similarity_tags*0.7 + similitud_age*0.3
         else:
-            similaruty_total = similarity_tags
+            similarity_total = similarity_tags
 
-        similarity_list[user.id] = similaruty_total
+        similarity_list[user.id] = similarity_total
 
 
     return similarity_list
@@ -60,12 +58,8 @@ def recommend_similar_users(similarity_matrix, n=10):
     # Obtener los usuarios más similares
     similar_users = sorted(similarity_matrix.items(), key=lambda x: x[1], reverse=True)[:n]
     # Crear una lista de usuarios similares con sus respectivas puntuaciones de similitud
-    recommended_users = []
-    print(similar_users)
-    for tuple in similar_users:
-        recommended_users.append(FlatterUser.objects.get(id=tuple[0]))
+    return [FlatterUser.objects.get(id=tuple[0]) for tuple in similar_users]
 
-    return recommended_users
 
 
 
