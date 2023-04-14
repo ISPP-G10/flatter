458896cccd2707cda60a26b5a1f604e4023d0a96
Alex-GF <yalejandro9@gmail.com>
2023-04-14 12:57:52 +0200

    FEAT: MOVIMIENTO EN CARRUSEL

--- client/src/libs/functions/customAlert.js
+++ client/src/libs/functions/customAlert.js
@@ -1,70 +1,77 @@
-import {useEffect, useRef} from "react";
+import { useEffect, useRef } from "react";
 
 import FlatterModal from "../../components/flatterModal";
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom/client";
 import { toast } from "react-hot-toast";
 
-const alert = ReactDOM.createRoot(document.getElementById('alert'));
-let environment = process.env.NODE_ENV
+const alert = ReactDOM.createRoot(document.getElementById("alert"));
+let environment = process.env.NODE_ENV;
 
-function customAlert(string, type='native', top=true) {
+function customAlert(string, type = "native", top = true) {
+  if (environment === "production") {
+    switch (type) {
+      case "error":
+        toast.error(
+          "Se ha producido un error.\nSi el problema persiste, contacte con el equipo de soporte.",
+          { position: top === true ? "top-center" : "bottom-center" }
+        );
+        break;
+      case "success":
+        toast.success(string, {
+          position: top === true ? "top-center" : "bottom-center",
+        });
+        break;
+      case "warning":
+        toast(string, {
+          icon: "⚠️",
+          position: top === true ? "top-center" : "bottom-center",
+          duration: 10000,
+        });
+        break;
+      case "info":
+        toast(string, {
+          icon: "ℹ️",
+          position: top === true ? "top-center" : "bottom-center",
+        });
+        break;
+      default:
+        const Alert = () => {
+          const alertRef = useRef(null);
 
-    switch (environment) {
-        case "production":
-            switch(type){
-                case "error":
-                    toast.error("Se ha producido un error.\nSi el problema persiste, contacte con el equipo de soporte.", {position: top===true?"top-center":"bottom-center"});
-                    break;
-                case "success":
-                    toast.success(string, {position: top===true?"top-center":"bottom-center"});
-                    break;
-                case "warning":
-                    toast(string, {icon : "⚠️", position: top===true?"top-center":"bottom-center", duration: 10000});
-                    break;
-                case "info":
-                    toast(string, {icon : "ℹ️", position: top===true?"top-center":"bottom-center",});
-                    break;
-                default:
-                    const Alert = () => {
+          useEffect(() => {
+            alertRef.current.open();
+          }, []);
 
-                        const alertRef = useRef(null);
-                
-                        useEffect(() => {
-                            alertRef.current.open();
-                        }, []);
-                
-                        return (
-                            <FlatterModal maxHeight={250} ref={alertRef} border={"2px solid black"}>
-                                <h3 style={{textAlign: 'center'}}>{string}</h3>
-                            </FlatterModal>
-                        );
-                    }
-                
-                    alert.render(<Alert/>);
-                    break;
+          return (
+            <FlatterModal
+              maxHeight={250}
+              ref={alertRef}
+              border={"2px solid black"}
+            >
+              <h3 style={{ textAlign: "center" }}>{string}</h3>
+            </FlatterModal>
+          );
+        };
 
-            }
-            break;
-        default:
-            const Alert = () => {
-
-                const alertRef = useRef(null);
-        
-                useEffect(() => {
-                    alertRef.current.open();
-                }, []);
-        
-                return (
-                    <FlatterModal maxHeight={250} ref={alertRef} border={"2px solid black"}>
-                        <h3 style={{textAlign: 'center'}}>{string}</h3>
-                    </FlatterModal>
-                );
-            }
-        
-            alert.render(<Alert/>);
-            break;
+        alert.render(<Alert />);
     }
+  } else {
+    const Alert = () => {
+      const alertRef = useRef(null);
 
+      useEffect(() => {
+        alertRef.current.open();
+      }, []);
+
+      return (
+        <FlatterModal maxHeight={250} ref={alertRef} border={"2px solid black"}>
+          <h3 style={{ textAlign: "center" }}>{string}</h3>
+        </FlatterModal>
+      );
+    };
+
+    alert.render(<Alert />);
+  }
 }
 
 export default customAlert;
