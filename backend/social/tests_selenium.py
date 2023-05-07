bd6be47a444d1f843faae00d083e926dbdcfe502
juangarcia16 <juangarciaoliva163@gmail.com>
2023-05-07 20:37:36 +0200

    TEST: IMPLEMENTACIÓN DE TESTS END TO END
    SE HA REALIZADO TESTING END TO END CON EL OBJETIVO DE TESTEAR LOS CHATS:
    NOS LOGUEAMOS COMO UN USUARIO, ADQUIRIMOS EL PLAN AVANZADO PARA PODER ABRIR CHATS, BUSCAMOS A OTRO USUARIO E INCIAMOS UN CHAT. TRAS ESTO CIERRA SESIÓN.
    NOS LOGUEAMOS COMO EL OTRO USUARIO Y ABRIMOS EL CHAT PARA ASEGURAR QUE LOS MENSAJES HAN SIDO RECIBIDOS.

--- backend/social/tests_selenium.py
+++ backend/social/tests_selenium.py
@@ -7,6 +7,7 @@ from selenium import webdriver
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium.webdriver.common.action_chains import ActionChains
 
 logging.disable(logging.CRITICAL)
 
@@ -15,15 +16,23 @@ logging.disable(logging.CRITICAL)
 class SeleniumTestCase(StaticLiveServerTestCase):
     def setUp(self):
         super().setUp()
+
         options = webdriver.ChromeOptions()
+        # crear una instancia de ChromeDriver
         self.browser = webdriver.Chrome(options=options)
+        # maximizar la ventana del navegador
+        self.browser.maximize_window()
+        # esperar implicitamente 5 segundos antes de lanzar excepciones
         self.browser.implicitly_wait(5)
+
         self.browser.get('http://localhost:3000')
 
-        flatteruser = FlatterUser(username='user_test', password='password_test', email='email@test.com', first_name='first_name_test', last_name='last_name_test', is_active=True, is_staff=False, is_superuser=False)
+        flatteruser = FlatterUser(username='user_test', password='password_test', email='email@test.com', 
+            first_name='first_name_test', last_name='last_name_test', is_active=True, is_staff=False, is_superuser=False, flatter_coins=1000)
         flatteruser.save()
 
-        inquilinoUser = FlatterUser(username='user_test_inquilino', password='password_test', email='inquilinoemail@test.com', first_name='Inquilino', last_name='Prueba', is_active=True, is_staff=False, is_superuser=False)
+        inquilinoUser = FlatterUser(username='user_test_inquilino', password='password_test', email='inquilinoemail@test.com', 
+            first_name='Inquilino', last_name='Prueba', is_active=True, is_staff=False, is_superuser=False, flatter_coins=1000)
         inquilinoUser.save()
 
         #Registro de usuario propietario
@@ -64,10 +73,10 @@ class SeleniumTestCase(StaticLiveServerTestCase):
         FlatterUser.objects.get(username='user_test').delete()
         FlatterUser.objects.get(username='user_test_inquilino').delete()
 
-
+    
     def test_register(self):
+       # Los usuarios ya han sido credos en el setUp
        pass
-
 
     # Test de login
     def test_login(self):
@@ -138,8 +147,8 @@ class SeleniumTestCase(StaticLiveServerTestCase):
         # Comprobamos que se ha editado correctamente
         self.browser.get('http://localhost:3000/me')
         self.assertEqual(self.browser.find_element(By.ID, "firstName").get_attribute('value'), "Mrs")
+    
 
-    '''
     # Test de chats
     def test_chat(self):
         # Primero nos logueamos con el usuario inquilino
@@ -152,16 +161,87 @@ class SeleniumTestCase(StaticLiveServerTestCase):
         time.sleep(2)
 
         # Adquirimos el plan avanzado para crear chats
+        ### No funciona la pasarela de pago, así que no se pueden añadir fondos para adquirir el plan avanzado
+        ### Se han añadido manualmente los flattercoins necesarios para adquirir el plan avanzado
         
+        self.browser.find_element(By.XPATH, "//a[contains(.,'Planes')]").click()
+        time.sleep(2)
+        self.browser.find_element(By.XPATH, "//button[contains(.,'Activar')]").click()
+        time.sleep(2)
+        self.browser.find_element(By.XPATH, "//button[contains(.,'Continuar')]").click()
+        time.sleep(2)
+        self.browser.find_element(By.XPATH, "//div[contains(text(), 'Confirmar')]").click()
+        time.sleep(2)
+
+        self.browser.get('http://localhost:3000')
+        time.sleep(2)
+
+        # Hacemos click en cualquier punto de la pantalla para cerrar el modal
+        '''
+        modal = self.browser.find_element(By.CSS_SELECTOR, '.modal-content')
+        modal.click()
+        elemento_fondo = self.browser.find_element(By.CSS_SELECTOR, '.modal-backdrop')
+        posicion_fondo = elemento_fondo.location
+        tamano_fondo = elemento_fondo.size
+        punto_x = posicion_fondo['x'] + tamano_fondo['width'] + 10 # Suma un pequeño margen para asegurarte de que estás fuera del fondo
+        punto_y = posicion_fondo['y'] + tamano_fondo['height'] + 10 # Suma un pequeño margen para asegurarte de que estás fuera del fondo
+        ActionChains(driver).move_by_offset(punto_x, punto_y).click().perform()
+        '''
+
 
         # Buscamos usuarios para chatear
         self.browser.find_element(By.XPATH, "//a[contains(.,'Buscador de usuarios')]").click()
+        time.sleep(2)
 
+        # Buscamos la tarjeta en la que se encuentra el primer usuario que aparece en la lista y hacemos click sobre su imagen
+        try:
+            user_prop = self.browser.find_element(By.XPATH, "//div[@class='user-card']//div[@class='user-card__info']//p[contains(text(), '@user_test')]")
+            card_info = user_prop.find_element(By.XPATH, "./..")
+            user_card = card_info.find_element(By.XPATH, "./..")
+        except:
+            user_card = self.browser.find_element(By.XPATH, "//div[@class='user-card']")
+            
+        img_element = user_card.find_element(By.XPATH, ".//div[@class='user-card__avatar']")
+        img_element.click()
+        time.sleep(2)
 
-        #self.browser.find_element(By.XPATH, "//h4[contains(.,'Cerrar sesión')]").click()
+        # Dentro de los detalles del usuario, hacemos click en el botón de chat
+        chat_button = self.browser.find_element(By.XPATH, "//button[@title='Contacta con @user_test']")
+        chat_button.click()
+        time.sleep(2)
+
+        # Escribimos un mensaje en el chat
+        chat_input = self.browser.find_element(By.ID, "chatInput")
+        chat_input.send_keys("Esto es una prueba de chat")
+        time.sleep(2)
+        chat_input.send_keys(Keys.ENTER)
+        time.sleep(2)
+
+        # Cerramos el chat y cerramos sesión
+        close_button = self.browser.find_element(By.CLASS_NAME, "class-close-chat")
+        close_button.click()
+        time.sleep(2)
+        self.browser.get('http://localhost:3000/me')
+        self.browser.find_element(By.XPATH, "//h4[contains(.,'Cerrar sesión')]").click()
+        time.sleep(2)
+
+        # Nos logueamos con el usuario propietario
+        self.browser.find_element(By.XPATH, "//button[contains(.,'Acceder')]").click()
+        self.browser.find_element(By.ID, "username").send_keys("user_test")
+        self.browser.find_element(By.ID, "password").send_keys("password_test")
+        self.browser.find_element(By.ID, "password").send_keys(Keys.ENTER)
+        time.sleep(2)
+
+        # Hacemos click en el icono de chats y abrimos el mensaje que nos ha enviado el usuario inquilino
+        chat_btn = self.browser.find_element(By.CSS_SELECTOR, '.class-chat-btn')
+        chat_btn.click()
+        time.sleep(2)
+        chat_group_element = self.browser.find_element(By.CLASS_NAME, "class-chat-group")
+        chat_group_element.click()
         time.sleep(3)
 
-    '''
+
+    
 
 
 
