97eae2b864e68c8be69d015aa3de2e43c662927c
Alex-GF <yalejandro9@gmail.com>
2023-05-09 19:33:54 +0200

    FIX: TERRAFORM VARS

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "google" {
-  credentials = "${var.json_credentials}"
+  credentials = file("${var.json_credentials}")
 
   project = var.project_id
   region  = var.region
